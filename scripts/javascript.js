3275196febd9189c3723190c62f2334a211f8d04
Harliamonti <123170910+Harliamonti@users.noreply.github.com>
2023-02-03 09:11:59 +0200

    Add variable examples

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,5 +1,14 @@
 console.log("Hello World but from an external script!")
 
+// Calculations
 console.log(24 + 97)
 console.log(2 + 4 + 8 + 16 + 32)
 console.log((4 + 6 + 9) / 77)
+
+
+// Variables
+let a = 10
+console.log(a)
+console.log(9 * a)
+let b = 7 * a
+console.log(b)
